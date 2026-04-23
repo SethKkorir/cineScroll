e77d0ca281654fcb80dc9456255e1a89ebed8eaf
SethKkorir <zsethkipchumba179@gmail.com>
2026-04-23 10:44:00 +0300

    15th commit as i finalize before presentation

---
+++ lib/views/main_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'home_feed_screen.dart';
+import 'watchlist_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const HomeFeedScreen(),
+    const Center(child: Text("Discover Coming Soon", style: TextStyle(color: Colors.white))),
+    const WatchlistScreen(),
+    const Center(child: Text("Profile Coming Soon", style: TextStyle(color: Colors.white))),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.white54,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.explore_outlined), label: "Discover"),
+          BottomNavigationBarItem(icon: Icon(Icons.bookmark_outline), label: "Watchlist"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
